f3d000fe69f4f57b5919c1f92d59f19f66c67ed3
vydidy <vydidy@gmail.com>
2024-09-24 17:26:36 +0200

    feat: implement styling and accordion functionality in FAQ section

--- script.js
+++ script.js
@@ -31,6 +31,21 @@ $(document).ready(function() {
     
 });
 
+var acc = document.getElementsByClassName("accordion-header");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
 // $(document).ready(function(){
 //     $('.reviews-container').slick({
 //       slidesToShow: 3,
